3f6f83c30e299f16c8463116d4f3b4772ac2859d
DavideAtzo <atzo9477@gmail.com>
2023-03-08 16:12:32 +0100

    array invitati

---
+++ es. snack1/js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const invitati = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
+
+
