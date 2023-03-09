e1fd2b0addf4acb01a34fb2e89f438807ec0c0df
DavideAtzo <atzo9477@gmail.com>
2023-03-09 17:06:47 +0100

    fix estrapolazione

--- es. snack3/js/script.js
+++ es. snack3/js/script.js
@@ -1,42 +1,35 @@
 'use strict';
 // array bicilette 
 const shopBike =
-[
-{ 
-    name: 'Bmx',
-    weight : '8'
-},
-{
-    name: 'Tandem',
-    weight : '9,5'
-},
-{
-    name: 'Bici ibrida',
-    weight : '9'
- },
- {
-    name: 'Bici per bambini',
-    weight : '7'
- },
- {
-    name: 'Triciclo',
-    weight : '8'
-},
-{
-    name: 'Mountain bike',
-    weight : '9'
-}
-];
-// estrapolo elementi dall'array 
-let [{weight}] = shopBike;
-let [{name}] = shopBike;
-// ciclo sull'array per trovare la bici più leggera
+    [
+        {
+            name: 'Bmx',
+            weight: '8'
+        },
+        {
+            name: 'Tandem',
+            weight: '9,5'
+        },
+        {
+            name: 'Bici ibrida',
+            weight: '9'
+        },
+        {
+            name: 'Bici per bambini',
+            weight: '7'
+        },
+        {
+            name: 'Triciclo',
+            weight: '8'
+        },
+        {
+            name: 'Mountain bike',
+            weight: '9'
+        }
+    ];
+// estrapolo con un ciclo gli elementi dall'array 
 shopBike.forEach((element) => {
-    if(element.weight < weight){
-        weight = element.weight;
-    }
+    let { name, weight } = element;
+    console.log(name, weight)
 });
 
-console.log(weight)
-console.log(name)
-console.log(`Drin drin !! compra questa ${name}, con un peso di ${weight}kg`);
