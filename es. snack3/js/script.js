a390018ead1b56c95bd5ed5c233de714b835cfb9
DavideAtzo <atzo9477@gmail.com>
2023-03-09 17:44:40 +0100

    fix numeri, ora prende il più leggero

--- es. snack3/js/script.js
+++ es. snack3/js/script.js
@@ -4,27 +4,27 @@ const shopBike =
     [
         {
             name: 'Bmx',
-            weight: '8'
+            weight: 8
         },
         {
             name: 'Tandem',
-            weight: '9,5'
+            weight: 9.5
         },
         {
             name: 'Bici ibrida',
-            weight: '9'
+            weight: 9
         },
         {
             name: 'Bici per bambini',
-            weight: '7'
+            weight: 7
         },
         {
             name: 'Triciclo',
-            weight: '8'
+            weight: 8
         },
         {
             name: 'Mountain bike',
-            weight: '9'
+            weight: 9
         }
     ];
 // estrapolo con un ciclo gli elementi dall'array 
@@ -33,12 +33,12 @@ shopBike.forEach((element) => {
     // console.log(name, weight)
 });
 // ciclo di bici e inserite in un array 
-let lightBike = shopBike.map((element) => {
+const lightBike = shopBike.map((element) => {
     const weight = element.weight;
     return weight;
 });
 
 console.log(lightBike)
 
-const ligherBike = Math.min(...lightBike)
-console.log(ligherBike)
+const lighterBike = Math.min(...lightBike)
+console.log(lighterBike)
