ea3df7ddba3df06f76aa97160b21d4dc127c970a
DavideAtzo <atzo9477@gmail.com>
2023-03-09 19:03:29 +0100

    correzione della chiave in falli (subiti)

--- es. snack4/js/script.js
+++ es. snack4/js/script.js
@@ -10,53 +10,48 @@ const serieA =
 { 
     team: 'Milan',
     Pt : 0,
-    GC : 0
+    faults : 0
 },
 {
     name: 'Juventus',
     Pt : 0,
-    GC : 0
+    faults : 0
 },
 {
     name: 'Napoli',
     Pt : 0,
-    GC : 0
+    faults : 0
  },
  {
     name: 'Inter',
     Pt : 0,
-    GC : 0
+    faults : 0
  },
  {
     name: 'Roma',
     Pt : 0,
-    GC : 0
+    faults : 0
 },
 {
     name: 'Lazio',
     Pt : 0,
-    GC : 0
+    faults : 0
 },
 {
     name: 'Torino',
     Pt : 0,
-    GC : 0
+    faults : 0
 }
 ]
 // estrapolo con un ciclo i punti fatti e gol subiti dall'array
 serieA.forEach((element) => {
-    let { Pt, GC } = element;
-    console.log(Pt, GC)
+    let { Pt, faults } = element;
+    console.log(Pt, faults)
 });
 // ciclo i punti fatti per inserirle in un array 
-const points = serieA.map(element => {
+const bestTeam = serieA.map(element => {
     element.Pt = getRandomInt(100);
-    return element.Pt
+    element.faults = getRandomInt(90);
+    return (element.Pt, element.faults)
 })
-console.log(points)
-
-const golConceded = serieA.map((element) => {
-    element.GC = getRandomInt(90);
-    return element.GC;
-});
-console.log(golConceded)
+console.log(bestTeam)
