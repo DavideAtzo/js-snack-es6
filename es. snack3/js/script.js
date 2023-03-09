315f34ae38611f79c716be1a883596d5f78b640b
DavideAtzo <atzo9477@gmail.com>
2023-03-09 15:45:34 +0100

    array bici

--- es. snack3/js/script.js
+++ es. snack3/js/script.js
@@ -1,3 +1,32 @@
 'use strict';
 
-alert('drin drin')
+const shopBike =
+[
+{ 
+    name: 'Bmx',
+    weight : '8'
+},
+{
+    name: 'Tandem',
+    weight : '9,5'
+},
+{
+    name: 'Bici ibrida',
+    weight : '10'
+ },
+ {
+    name: 'Bici per bambini',
+    weight : '7'
+ },
+ {
+    name: 'Triciclo',
+    weight : '8'
+},
+{
+    name: 'Mountain bike',
+    weight : '9'
+}
+];
+
+console.log(shopBike)
+
