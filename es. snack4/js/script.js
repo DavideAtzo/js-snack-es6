ee17e097ef60911572a47228c363ea8c58411c6d
DavideAtzo <atzo9477@gmail.com>
2023-03-09 18:22:32 +0100

    array serie A

--- es. snack4/js/script.js
+++ es. snack4/js/script.js
@@ -1,3 +1,40 @@
 'use strict';
 
-alert('goooooooooooooooooooooool')
+const serieA =
+[
+{ 
+    team: 'Milan',
+    Pt : 0,
+    GC : 0
+},
+{
+    name: 'Juventus',
+    Pt : 0,
+    GC : 0
+},
+{
+    name: 'Napoli',
+    Pt : 0,
+    GC : 0
+ },
+ {
+    name: 'Inter',
+    Pt : 0,
+    GC : 0
+ },
+ {
+    name: 'Roma',
+    Pt : 0,
+    GC : 0
+},
+{
+    name: 'Lazio',
+    Pt : 0,
+    GC : 0
+},
+{
+    name: 'Torino',
+    Pt : 0,
+    GC : 0
+}
+]
